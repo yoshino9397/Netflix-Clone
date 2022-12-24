9da779bbfb2685e19b214b80b5cad41e17bcd15a
yoshino9397 <yosino8512@gmail.com>
2022-12-24 00:24:00 -0800

    header basement DONE

--- myapp/components/Header.tsx
+++ myapp/components/Header.tsx
@@ -1,10 +1,9 @@
-import Head from "next/head";
 import Image from "next/image";
 
 const Header = () => {
   return (
     <header>
-      <div>
+      <div className="flex items-center space-x-2 md:space-x-10">
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
           alt="logo"
@@ -12,6 +11,13 @@ const Header = () => {
           height={100}
           className="cursor-pointer object-contain"
         />
+        <ul className="hidden space-x-4 md:flex">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+        </ul>
       </div>
       <div></div>
     </header>
--- myapp/pages/index.tsx
+++ myapp/pages/index.tsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center py-2">
+      <div className="">
         <Head>
           <title>HOME</title>
           <link rel="icon" href="/favicon.ico" />
