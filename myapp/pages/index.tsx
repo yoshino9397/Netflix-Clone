6727bef1f4cf4b8cf4b627789249766abcfb47e8
yoshino9397 <yosino8512@gmail.com>
2023-01-13 22:20:04 -0800

    header DONE

--- myapp/pages/index.tsx
+++ myapp/pages/index.tsx
@@ -1,18 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="">
+      <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511]  lg:h-[140vh]">
         <Head>
           <title>HOME</title>
-          <link rel="icon" href="/favicon.ico" />
         </Head>
         <Header />
         <main>
-          {/* Banner */}
+          <Banner />
           <section>
             {/* Banner */}
             {/* Banner */}
