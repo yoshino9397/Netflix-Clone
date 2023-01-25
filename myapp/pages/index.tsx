4f0d028a8fe1695cdf696194cc6f21a16a3d0222
yoshino9397 <yosino8512@gmail.com>
2023-01-24 21:12:40 -0800

    Thumbnail DONE

--- myapp/pages/index.tsx
+++ myapp/pages/index.tsx
@@ -3,6 +3,7 @@ import Banner from "../components/Banner";
 import Header from "../components/Header";
 import requests from "../utils/requests";
 import { Movie } from "../typings";
+import Row from "../components/Row";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -34,15 +35,18 @@ Props) => {
           <title>HOME</title>
         </Head>
         <Header />
-        <main>
+        <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
           <Banner netflixOriginals={netflixOriginals} />
           <section>
-            {/* Banner */}
-            {/* Banner */}
-            {/* Banner */}
-            {/* Banner */}
-            {/* Banner */}
-            {/* Banner */}
+            <Row title="Trending Now" movies={trendingNow} />
+            <Row title="Top Rated" movies={topRated} />
+            <Row title="Action Thrillers" movies={actionMovies} />
+            {/* My List */}
+
+            <Row title="Comedies" movies={comedyMovies} />
+            <Row title="Scary Movies" movies={horrorMovies} />
+            <Row title="Romance Movies" movies={romanceMovies} />
+            <Row title="Documentaries" movies={documentaries} />
           </section>
         </main>
         {/* modal */}
